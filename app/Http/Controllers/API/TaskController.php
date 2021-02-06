1fb1249ee48ee98b018743084775fa8344bfac34
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-02-06 22:02:19 +0100

    initial commit

---
+++ app/Http/Controllers/API/TaskController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\TaskResource;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $tasks = Task::all();
+        return response([ 'tasks' => TaskResource::collection($tasks), 'message' => 'Task Retrieved'], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'title' => 'required|max:255',
+            'description' => 'required|max:255'
+        ]);
+
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors(), 'Validation Error']);
+        }
+
+        $task = Task::create($data);
+
+        return response(['task' => new TaskResource($task), 'message' => 'Task Created'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+        return response(['task' => new TaskResource($task), 'message' => 'Task Retrieved'], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        //
+        $task->update($request->all());
+
+        return response(['task' => new TaskResource($task), 'message' => 'Task Updated'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+        $task->delete();
+
+        return response(['message' => 'Task Deleted']);
+    }
+}
